642d2b6b78212cd4644e26761efbb10bebbeca85
Lucas Prado <lukep.onbass@gmail.com>
2021-12-15 10:07:43 -0300

    Changing the http status

--- src/controllers/CreateOccurrenceController.ts
+++ src/controllers/CreateOccurrenceController.ts
@@ -5,14 +5,14 @@ class CreateOccurrenceController {
     async handle(request: Request, response: Response) {
         const occurrence = request.body;
         const files = <Express.Multer.File[]>request.files;
-        
+
         const service = new CreateOccurenceService();
 
         const result = await service.execute(occurrence, files).catch((e) => {
-            return response.status(500).send('Something went wrong');
+            return response.status(500).json({ message: 'Somenthing went wrong' });
         });
-        
-        return response.json(result);
+
+        return response.status(201).json(result);
     }
 }
 
