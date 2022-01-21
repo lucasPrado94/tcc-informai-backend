7e45978a0a5216ecc1b68ae0704886d4fa446c6e
Lucas Prado <lukep.onbass@gmail.com>
2022-01-21 13:01:13 -0300

    Increasing max file size again

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -6,7 +6,7 @@ import multerS3 from 'multer-s3'
 export default {
     dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
     limits: {
-        fileSize: 10 * 1024 * 1024
+        fileSize: 80 * 1024 * 1024
     },
     storage: multerS3({
         s3: new aws.S3({
